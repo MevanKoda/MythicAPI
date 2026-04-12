ac52543e0e9cbc5b4c99a1f597425cf2d82ffade
Mevan Kodagoda <136101449+MevanKoda@users.noreply.github.com>
2026-04-12 15:07:10 +0530

    endpoints created with database connection

--- server.js
+++ server.js
@@ -1,14 +1,116 @@
 import express from 'express'
-const PORT = 8000
+import pool from './db/database.js'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
 app.use(express.json())
 
-app.get('/mythologies', (req,res)=>{
-    res.json({message: "Here are your mythologies response"})
+
+app.get('/v1/',(req,res)=>{
+
+    const query = `SELECT json_agg(json_build_object(
+	'mythology_name', title,
+	'region', region,
+	'description', description,
+	'time_start', time_start,
+	'time_end', time_end,
+	'creation_myth', creation_myth_story,
+	
+	--gods data object
+	'gods', COALESCE((SELECT(json_agg(
+		json_build_object(
+			'name', g.name,
+			'alternative_names', g.alternative_names,
+			'description', g.description,
+			'origin_story', g.origin_story,
+			'deity_type', g.deity_type,
+			'role', g.role,
+			'tier', g.tier,
+			'gender', g.gender,
+			'powers', g.powers,
+			'symbols', g.symbols,
+			'weaknesses', g.weaknesses,
+			'is_defied', g.is_deified,
+			'defied_by', g.deified_by,
+			'preview_img', g.preview_img
+		)
+	))FROM god g WHERE g.myth_id = myth.id), '[]'::json),
+		
+
+	--creature data object
+	'creatures & Monsters', COALESCE((SELECT(json_agg(
+		json_build_object(
+			'name' , c.name,
+			'alternative_names', c.alternative_names,
+			'created_by', c.created_by,
+			'description', c.description,
+			'creature_type', c.creature_type,
+			'material_nature', c.material_nature,
+			'origin_story', c.origin_story,
+			'powers', c.powers,
+			'weaknesses', c.weaknesses,
+			'is_mortal', c.is_mortal,
+			'alignment', c.alignment,
+			'killed_by' , c.killed_by,
+			'preview_img', c.preview_img
+		)
+	))FROM creature c WHERE c.myth_id = myth.id), '[]'::json)
+
+))FROM myth;
+`
+    pool.query(query, (err,result)=>{
+        if(!err){
+            const data =  result.rows[0].json_agg
+            res.json(data)
+        }else{
+            res.json({message:"Error fetching data"})
+        }
+    })
+
+})
+
+app.get('/v1/mythologies',(req,res)=>{
+
+    pool.query(`SELECT * FROM myth;`, (err,result)=>{
+        if(!err){
+            const data =  result.rows
+            res.json(data)
+        }else{
+            res.json({message:"Error fetching data"})
+        }
+    })
+
 })
 
 
-app.listen(PORT,(req,res)=>{
+app.get('/v1/gods',(req,res)=>{
+
+    pool.query(`SELECT * FROM god;`, (err,result)=>{
+        if(!err){
+            const data =  result.rows
+            res.json(data)
+        }else{
+            res.json({message:"Error fetching data"})
+        }
+    })
+
+})
+
+app.get('/v1/creatures',(req,res)=>{
+
+    pool.query(`SELECT * FROM creature;`, (err,result)=>{
+        if(!err){
+            const data =  result.rows
+            res.json(data)
+        }else{
+            res.json({message:"Error fetching data"})
+        }
+    })
+
+})
+
+
+app.listen(process.env.PORT,(req,res)=>{
     console.log("Server is running✅")
 })
